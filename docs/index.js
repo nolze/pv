e6968d11d09156d7442cf81a8cd389e919cb50f3
nolze <nolze@archlinux.us>
2017-03-27 21:29:30 +0900

    Remove some debug lines

--- docs/index.js
+++ docs/index.js
@@ -7345,15 +7345,13 @@ var md = require('markdown-it')()
 var mdurl = location.hash.substr(1)
 
 if (mdurl != "") {
-  console.log(mdurl)
   fetch(mdurl).then(function (response) {
-    console.log(response)
     return response.text()
   }).then(function (text) {
     console.log(text)
     if (text == "") return
     var html = md.render(text)
-    console.log(html)
+    // console.log(html)
     var div = document.getElementById('main-wrapper')
     div.innerHTML = html
     document.title = (new URL(mdurl)).pathname.split('/').slice(-1)[0]
